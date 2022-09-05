0c1295cabe16f186792d951e6d309b31e5df7abe
Phat Panhareach <panhareach.phat@student.cadt.edu.kh>
2022-09-05 14:11:15 +0700

    login & logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,21 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useContext } from 'react'
+import AuthContext from '../stores/authContext'
 
 export default function Navbar() {
+  const {user, login, logout} = useContext(AuthContext)
+
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
-        <h1>Gaming Vibes</h1>
+        <h1>Dojo Games</h1>
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li onClick={login} className="btn">Login/Signup</li>
+          <li onClick={logout} className="btn">Log Out</li>
         </ul>
       </nav>
       <div className="banner">
